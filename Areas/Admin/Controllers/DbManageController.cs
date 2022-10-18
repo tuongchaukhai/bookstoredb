a9b06a4c8428aa82079a63bac765cc31cf8f5a9c
tuong.chaukhai <tuong.chaukhai@gmail.com>
2022-10-18 10:36:38 +0700

    Added Product Model

---
+++ Areas/Admin/Controllers/DbManageController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace bookstore.Areas.Admin.Controllers
+{
+    [Route("/database-manage/[action]")]
+    public class DbManageController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public DbManageController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DeleteDb()
+        {
+            return View();
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteDbAsync()
+        {
+            var success = await _dbContext.Database.EnsureDeletedAsync();
+            StatusMessage = success ? "Xóa Database thành công" : "Không xóa được";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Migrate()
+        {
+            await _dbContext.Database.MigrateAsync();
+
+            StatusMessage = "Cập nhật Database thành công";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
